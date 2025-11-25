a5094e2da6b81e4157fd5fdf145cff861299ab77
Len.icon <lenthesmiledev@gmail.com>
2025-11-25 19:18:53 +0800

    PLEASE PULL THIS, then design it cuz i wanna start adding in the code

--- App/src/Main.java
+++ App/src/Main.java
@@ -4,17 +4,21 @@ import javax.swing.border.EmptyBorder;
 public class Main extends JFrame{
     private JPanel mainPane;
 
-    public Main(){
+    public Main() {
 
-        setContentPane(panelMain);
-        setTitle("Character Counter (Len.icon)");
+        setContentPane(mainPane);
+        setTitle("Project Task Manager");
         pack();
 //        setSize(600, 400);
         setMinimumSize(getSize());
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater ( () -> new Main());
+
+        SwingUtilities.invokeLater(() -> new Main());
+
     }
+
 }
