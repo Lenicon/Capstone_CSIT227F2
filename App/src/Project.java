03863c66fc7fec728069c0d0f2d021038c399013
Len.icon <lenthesmiledev@gmail.com>
2025-12-02 16:32:22 +0800

    update project

--- App/src/Project.java
+++ App/src/Project.java
@@ -1,11 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 class Project {
     String name;
     List<Task> tasks = new ArrayList<>();
+    int id;
 
-    Project(String name) { this.name = name; }
+    Project(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+    Project(String name) {
+        this(name, new Random().nextInt());
+    }
+
+    public String fileName() {
+        return name + "_" + id + ".dat";
+    }
 
     @Override
     public String toString() { return name; }
