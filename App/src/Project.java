3d7ae5839c670471226e7a993582b7ac85647772
Len.icon <lenthesmiledev@gmail.com>
2025-12-11 19:12:47 +0800

    removed some warnings

--- App/src/Project.java
+++ App/src/Project.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.UUID;
 import java.time.Instant;
 
-class Project implements Serializable {
+public class Project implements Serializable {
     private static final long serialVersionUID = 3L;
 
     List<Task> tasks = new ArrayList<>();
